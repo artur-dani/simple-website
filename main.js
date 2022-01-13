7ed900382adb2876d03c556a3bc8477a3a5bc082
Artur Dani <darthur83@hotmail.com>
2022-01-13 17:52:31 +0100

    add navbar effect on scroll

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ $menuToggler.addEventListener("click", function () {
   $menuToggler.classList.toggle("is-active");
   $navbarList.classList.toggle("is-shown");
 });
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 70) {
+    navbar.classList.add("navbar--shadowed");
+  } else {
+    navbar.classList.remove("navbar--shadowed");
+  }
+});
